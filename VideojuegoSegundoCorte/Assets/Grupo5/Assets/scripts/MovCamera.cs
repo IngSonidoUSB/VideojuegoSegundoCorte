06fd8b543ed12c6f21c338844a7ab517d61850ac
angarce1994 <angarce1994@hotmail.com>
2016-04-18 19:57:20 -0500

    PRIMEROS AVANCES GRUPO 5
    La programación del movimiento de la cámara fue realizado por Natalia
    Murcia
    El diseño del laberinto fue desarrollado por Nathalia Beltrán y Angie
    Guerrero
    La programación del poder fue desarrollado por Angie Guerrero

---
+++ VideojuegoSegundoCorte/Assets/Grupo5/Assets/scripts/MovCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using CnControls;
+
+//Programado por Natalia Murcia
+public class MovCamera : MonoBehaviour
+{
+
+    public float lookSpeed = 10;
+    private Vector3 curLoc;
+    private Vector3 prevLoc;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        float moveInputh = CnInputManager.GetAxis("Horizontal") * Time.deltaTime * 80;
+
+        transform.Rotate(new Vector3(0, moveInputh, 0), Space.Self);
+
+        float moveInputv = CnInputManager.GetAxis("Vertical") * Time.deltaTime * 10;
+        transform.Translate(Vector3.forward * moveInputv);
+
+    }
+
+
+
+
+}
---
+++ VideojuegoSegundoCorte/Assets/Grupo5/Assets/scripts/poder.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class poder : MonoBehaviour
+{
+//DESARROLLADO POR: Angie Natalia Guerrero
+
+    float Parpadeo = 0.1f;
+    
+    float val = 0.35f;
+    float ran = 1f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+       if(Input.GetKeyDown("space"))
+        {
+            StartCoroutine("WaitThreeSeconds");
+
+        }
+
+    }
+
+     IEnumerator WaitThreeSeconds()
+    {
+        for (int i= 0; i < 6; i++)
+        {
+
+            GetComponent<Light>().enabled = false;
+            yield return new WaitForSeconds(Parpadeo);
+            GetComponent<Light>().enabled = true;
+            GetComponent<Light>().intensity = val;
+            GetComponent<Light>().range = ran;
+            yield return new WaitForSeconds(Parpadeo);
+            val = val + 0.35f;
+            ran = ran + 1f;
+        }
+        yield return new WaitForSeconds(5);
+        for (int o = 1; o < 15; o++)
+        {
+            val = val - 0.15f;
+            ran = ran - 0.5f;
+            GetComponent<Light>().intensity = val;
+            GetComponent<Light>().range = ran;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+        
+    
+}
+    
+
