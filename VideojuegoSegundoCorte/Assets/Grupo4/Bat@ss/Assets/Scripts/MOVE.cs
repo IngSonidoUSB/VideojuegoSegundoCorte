82842a7c2e9bdf18bc4423d83d28b8601e2a4be6
AlexRamirezM <AlexRamirez@Alex.local>
2016-05-05 14:03:46 -0500

    Grupo 4 -  Batass + FMOD P1

---
+++ VideojuegoSegundoCorte/Assets/Grupo4/Bat@ss/Assets/Scripts/MOVE.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MOVE : MonoBehaviour {
+
+	[FMODUnity.EventRef]
+	public string Poder_Path = "event:/Grupo 4/Poder";
+
+	public ElementoInteractivo Poder;
+	public ElementoInteractivo Aceler;
+	public Scrollbar Barra;
+	public GameObject Winn;
+
+	public bool SPACE = false;
+
+	public float Mov_Speed = 1;
+	Light Ecoloc;
+	public bool Colision;
+	float PP=1;
+
+		void Start () {
+
+		Ecoloc = GetComponent<Light> ();
+		Barra = GameObject.Find ("HealthBar").GetComponent<Scrollbar>();
+	
+		}
+
+		void Update(){
+
+
+		transform.Rotate (0.0f, Input.GetAxis("Horizontal") * 3, 0.0f);
+
+		if(Aceler.pulsado) {
+			
+			transform.position = transform.position + transform.forward * Mov_Speed;
+
+		} else if (Input.GetKey(KeyCode.S)) {
+
+			transform.position = transform.position - transform.forward * Mov_Speed/2;
+
+		}
+
+		if ((Input.GetKey (KeyCode.Space) == false || Poder.pulsado == false) && PP < 1) { 
+			PP = PP + 0.0005f;
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) == true) {
+		
+			FMODUnity.RuntimeManager.PlayOneShot (Poder_Path, transform.position);
+			SPACE = true;
+		}
+
+		Barra.size = PP;
+
+		if ((Input.GetKey (KeyCode.Space) || Poder.pulsado) && PP > 0.01) {
+
+
+			PP = PP - 0.01f;
+			if (PP < 0) {
+				PP = 0;
+			}
+
+			if (Ecoloc.intensity <= 8) { 
+				
+				Ecoloc.intensity = Ecoloc.intensity + 0.1f;
+			}
+		
+		} else {
+
+			if (Ecoloc.intensity > 0) {
+
+			
+				Ecoloc.intensity = Ecoloc.intensity - 1;
+			} 
+		
+		}
+			
+	}
+
+	void OnTriggerEnter(Collider Col) {
+		if (Col.gameObject.tag == "Flies") {
+			Colision = true;
+			Destroy (Col.gameObject);
+			PP = PP + 0.15f;
+			if (PP > 1) {
+				PP = 1;
+			}
+		}
+			
+if(Col.gameObject.name == "Flies (13)" || Col.gameObject.name == "Flies (15)" || Col.gameObject.name == "Flies (16)" || Col.gameObject.name == "Flies (17)") {
+			Winn.SetActive (true);
+		}
+
+		if (Col.gameObject.tag == "Spider") {
+			SceneManager.LoadScene("Bat@ss");
+		}
+	}
+
+
+//	public void ChangeToScene (){
+//		Application.LoadLevel ("Bat@ss");
+//	}
+
+}
