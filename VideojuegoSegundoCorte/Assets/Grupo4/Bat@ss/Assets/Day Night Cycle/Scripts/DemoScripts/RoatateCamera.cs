b2fcbdafb899ec4058ae64fa23bdbd830dc82541
AlexRamirezM <AlexRamirez@Alex.local>
2016-05-05 12:08:32 -0500

    Grupo 4 - Bat@ss

---
+++ VideojuegoSegundoCorte/Assets/Grupo4/Bat@ss/Assets/Day Night Cycle/Scripts/DemoScripts/RoatateCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoatateCamera : MonoBehaviour {
+	//How Senstive the camera movement should be?
+
+	public float sensitivity = 5.0f;
+	public float AccX;
+	public float AccY;
+	float Y;
+	// Use this for initialization
+	void Start () {
+		Y = Input.acceleration.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		AccX = Input.acceleration.x;
+		AccY = Input.acceleration.y;
+
+	//Rotate camera according to input
+//		transform.eulerAngles += new Vector3 (-Input.GetAxis("Mouse Y") * senstivity, Input.GetAxis("Mouse X") * senstivity,0.0f);
+
+//		transform.Rotate (2*(AccY-Y), -3 * AccX, 0);
+
+		transform.Rotate (sensitivity  * AccY, sensitivity  * AccX,0);
+	}
+}
---
+++ VideojuegoSegundoCorte/Assets/Grupo4/Bat@ss/Assets/Spider.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spider : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject PlayerObject;
+    public Animation Animation;
+    public float PlayerDistance;
+    public float RotationSpeed = 0.001f;
+    public float MovementSpeed = 10;
+    private Quaternion LookRotation;
+    private Vector3 Direction;
+    private Vector3 TargetPosition;
+    
+
+	void Start () {
+
+        PlayerObject = GameObject.Find("Player");
+        Animation = GetComponent<Animation>();
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        
+
+        PlayerDistance = Vector3.Distance(PlayerObject.transform.position, transform.position);
+
+        if (PlayerDistance <  60) {
+
+            Animation.Play("Run"); 
+
+            Direction = new Vector3((PlayerObject.transform.position.x - transform.position.x), 0, (PlayerObject.transform.position.z - transform.position.z));
+
+            LookRotation = Quaternion.LookRotation(Direction);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, LookRotation, RotationSpeed);
+
+            TargetPosition = new Vector3(PlayerObject.transform.position.x, 0, PlayerObject.transform.position.z);
+
+            transform.position = Vector3.MoveTowards(transform.position, TargetPosition, Time.deltaTime * MovementSpeed);
+        } else
+        {
+            Animation.Play("Idle");
+        }
+
+
+        
+	
+	}
+}
